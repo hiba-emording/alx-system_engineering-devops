6dd6055aa2f1f018bff098a2e5e528dc2acd83d3
hiba-emording <hibaalaaeldeinmohamed@gmail.com>
2024-06-13 21:49:27 +0000

    updated

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,13 +4,10 @@ exec { 'replace_file_descriptor_limit':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart_nginx_service'],
-  unless   => 'grep -q "ULIMIT=\"-n 4096\"" /etc/default/nginx',
 }
 
 
 exec { 'restart_nginx_service':
-  provider    => shell,
-  command     => 'sudo service nginx restart',
-  refreshonly => true,
-  subscribe   => Exec['replace_file_descriptor_limit'],
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
